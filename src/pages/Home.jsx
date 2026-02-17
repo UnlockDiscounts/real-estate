84d1338d7e3e10af27c31eb466eae662c9b8a5a7
yagyasoni <yagyasony@gmail.com>
2026-02-17 18:32:37 +0530

    refactor Home component to use structured layout with sections and imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <div className="p-20 text-3xl">Home Page </div>;
-}
+
+import Navbar from '../components/Navbar.jsx';
+import Hero from '../components/Home/Hero.jsx';
+import Properties from '../components/Home/Properties.jsx';
+import Stats from '../components/Home/Stats.jsx';
+import MaterialSupply from '../components/Home/MaterialSupply.jsx';
+import WhyChoose from '../components/Home/WhyChoose.jsx';
+import Footer from '../components/Footer.jsx';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#F8FCFF]">
+      <Navbar />
+      <main className="flex-grow">
+        <section id="home">
+          <Hero />
+        </section>
+        
+        <section id="properties" className="py-16 md:py-24">
+          <Properties />
+        </section>
+
+        <section id="stats" className="py-12">
+          <Stats />
+        </section>
+
+        <section id="materials" className="py-16 md:py-24">
+          <MaterialSupply />
+        </section>
+
+        <section id="about" className="py-16 md:py-24">
+          <WhyChoose />
+        </section>
+      </main>
+      
+    </div>
+  );
+};
+
+export default Home;
